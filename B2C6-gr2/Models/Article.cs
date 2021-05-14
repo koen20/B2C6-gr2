ca883fc3294fd00e71ea5e8d4f9430067b764d8f
Koen Habets <koenhabets1@gmail.com>
2021-05-14 22:57:44 +0200

    Add project files.

---
+++ B2C6-gr2/Models/Article.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace B2C6_gr2.Models
+{
+    public class Article
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public decimal price { get; set; }
+
+    }
+
+    public class ArticleDBContext : DbContext
+    {
+        public DbSet<Article> Articles { get; set; }
+    }
+}
---
+++ B2C6-gr2Tests/Controllers/ArticlesControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using B2C6_gr2.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using B2C6_gr2.Models;
+
+namespace B2C6_gr2.Controllers.Tests
+{
+    [TestClass()]
+    public class ArticlesControllerTests
+    {
+        [TestMethod()]
+        public void CreateTest()
+        {
+            ArticlesController articlesController = new ArticlesController();
+            Assert.IsNotNull(articlesController.Index());
+        }
+    }
+}
