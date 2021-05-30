81c9d8e064991f76a3355b64c21d0b469e8f4e43
Koen Habets <koenhabets1@gmail.com>
2021-05-30 21:27:46 +0200

    Add unit test

---
+++ B2C6-gr2Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using B2C6_gr2.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B2C6_gr2.Controllers.Tests
+{
+    [TestClass()]
+    public class HomeControllerTests
+    {
+        [TestMethod()]
+        public void IndexTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
