d731a27aad941c26b4bfa1cd6e4acb0e3e1f0c09
Koen Habets <koenhabets1@gmail.com>
2021-06-09 17:38:50 +0200

    Add ui tests

---
+++ B2C6-gr2Tests/AutomatedUITests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B2C6_gr2Tests
+{
+    [TestClass]
+    public class AutomatedUITests : IDisposable
+    {
+        private readonly IWebDriver chromeDriver;
+        public AutomatedUITests()
+        {
+            //chromeDriver = new ChromeDriver(@"C:\Users\koenh\source\repos\B2C6-gr2\B2C6-gr2Tests\");
+            chromeDriver = new ChromeDriver();
+        }
+        public void Dispose()
+        {
+            chromeDriver.Quit();
+            chromeDriver.Dispose();
+        }
+
+        [TestMethod()]
+        public void Privacy()
+        {
+            chromeDriver.Navigate()
+                .GoToUrl("https://b2c6-gr2.azurewebsites.net/");
+            //Assert.Equals("", _driver.Title);
+            Assert.IsNotNull(chromeDriver.Title);
+        }
+    }
+}
