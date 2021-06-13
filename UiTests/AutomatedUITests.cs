975c09fb079d189ffb395f1cc07b7aa022b2afde
Koen Habets <koenhabets1@gmail.com>
2021-06-14 00:43:51 +0200

    Added ui test

--- UiTests/AutomatedUITests.cs
+++ UiTests/AutomatedUITests.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,8 +16,8 @@ namespace B2C6_gr2Tests
         private readonly IWebDriver chromeDriver;
         public AutomatedUITests()
         {
-            //chromeDriver = new ChromeDriver(@"C:\Users\koenh\source\repos\B2C6-gr2\B2C6-gr2Tests\");
-            chromeDriver = new ChromeDriver();
+            chromeDriver = new ChromeDriver(@"C:\Users\koenh\source\repos\B2C6-gr2\B2C6-gr2Tests\");
+            //chromeDriver = new ChromeDriver();
         }
         public void Dispose()
         {
@@ -25,21 +26,23 @@ namespace B2C6_gr2Tests
         }
 
         [TestMethod()]
-        public void Privacy()
-        {
-            chromeDriver.Navigate()
-                .GoToUrl("https://b2c6-dev.azurewebsites.net/");
-            //Assert.Equals("", _driver.Title);
-            Assert.IsNotNull(chromeDriver.Title);
-        }
-
-        [TestMethod()]
         public void Articles()
         {
             chromeDriver.Navigate()
                 .GoToUrl("https://b2c6-dev.azurewebsites.net/Articles");
-            //Assert.Equals("", _driver.Title);
             Assert.AreEqual("Artikelen catalogus - B2C6_gr2", chromeDriver.Title);
+        }
+
+        [TestMethod()]
+        public void ArticleSearch()
+        {
+            chromeDriver.Navigate()
+                .GoToUrl("https://b2c6-dev.azurewebsites.net/Articles");
+            chromeDriver.FindElement(By.Id("searchField")).SendKeys("Stoel");
+            chromeDriver.FindElement(By.ClassName("btn")).Click();
+            WebDriverWait wait = new WebDriverWait(chromeDriver, new System.TimeSpan(0, 1, 0));
+            var text = chromeDriver.FindElement(By.ClassName("grid-container"));
+            Assert.IsTrue(text.Text.Contains("Stoel"));
         }
     }
 }
